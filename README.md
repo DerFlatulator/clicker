173622465221f9600448accc78a313eb10cd31ed
Lucas Azzola <laazz1@student.monash.edu>
2015-05-18 21:01:46 +1000

    Make setup script more compliant Swith from || {} syntax to if; then; fi.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-### Clicker
+### Collaborative Clicker Interaction
+
+## Proof of Concept
+
+Created by Lucas Azzola.
+
+Installation steps (for development):
+
+1. clone this repostory 
+2. run `$ bash setup.py`
+3. set up a super-user for Django, if required
+4. run `$ python manage.py runserver`
+5. head to `http://localhost:8000/`
--- setup.sh
+++ setup.sh
@@ -4,23 +4,59 @@
 # TODO: Check if already installed before going ahead
 #
 
+python_module_installed() {
+    python -c "__import__('$1')"
+}
+
+npm_module_installed() {
+    hash $1 2>/dev/null
+}
+
+install_or_die_python() {
+    if ! python_module_installed "$1"; then
+        echo "Installing $1"
+        if ! sudo pip install Django ; then
+            echo "Failed to install $1"; exit
+        fi
+    fi
+}
+
+install_or_die_npm() {
+    if ! npm_module_installed "$1"; then
+        echo "Installing $1"
+        if ! sudo npm install "$1" -g; then
+            echo "Failed ot install $1"; exit
+        fi
+    fi
+}
+
 echo "Setting up, this may take a little while..."
 
-echo "Installing Django"
-sudo pip install Django || { echo "Failed to install Django"; exit }
-
-sudo pip install djangorestframework || { echo "Failed to install Django REST Framework"; exit }
+install_or_die_python "django"
+install_or_die_python "rest_framework"
 
 echo "Installing node modules"
-npm install || { echo "Failed to install npm modules"; exit }
+if ! npm install; then
+    echo "Failed to install npm modules"; exit
+fi
 
-echo "Installing bower"
-sudo npm install -g bower || { echo "Failed to install bower"; exit }
+install_or_die_npm "bower"
+install_or_die_npm "bower-installer"
+install_or_die_npm "gulp"
 
 echo "Installing bower components"
-bower install && bower-installer || { echo "Failed to install bower components"; exit }
+if ! bower install && bower-installer; then
+    echo "Failed to install bower components"; exit
+fi
 
 echo "Migrating database"
-python manage.py migrate || { echo "Failed to migrate database"; exit }
-#python manage.py createsuperuser
+if ! python manage.py migrate; then
+    echo "Failed to migrate database"; exit
+fi
 
+echo -n "Do you want to create a Django super-user? (yes/no) "
+read l; if [[ $l =~ "yes" ]] ; then
+    python manage.py createsuperuser
+fi
+
+gulp default
